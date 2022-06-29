15c711e77cc30634ade7d203a601820a3ddf44cf
Jorge Jimenez <ing_jorgejimenez@outlook.com>
2022-06-29 15:12:14 -0500

    estilos con bootstrap y organizacion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { getAllPokemon, getPokemon } from './services/pokemon';
+import Navbar from "./components/layout/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Sections from "./components/layout/Sections";
+import Sections from "./components/pokemon/PokemonCarta"
 import './App.css';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [previousUrl, setPreviousUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialUrl);
+      setNextUrl(response.next);
+      setPreviousUrl(response.previous);
+      let pokemon = await loadingPokemon(response.results);
+      console.log(pokemon);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  const next = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPreviousUrl(data.previous);
+    setLoading(false);
+  }
+
+  const prev = async () => {
+    if (!previousUrl) return;
+    setLoading(true);
+    let data = await getAllPokemon(previousUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPreviousUrl(data.previous);
+    setLoading(false);
+  }
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map(async pokemon => {
+        let pokemonRecord = await getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+
+    setPokemonData(_pokemonData);
+  };
+
+  // console.log(pokemonData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading ? <h1>Loading...</h1> : (
+        <>
+          <Navbar />
+          <div className="btn">
+            <button onClick={prev}>Prev</button>
+            <button onClick={next}>Next</button>
+          </div>
+          <div className="container">
+            <div className='row'>
+              {pokemonData.map((pokemon, i) => {
+
+                return <Sections key={i} pokemon={pokemon} />
+
+              })}
+            </div>
+          </div>
+          <div className="btn">
+            <button onClick={prev}>Prev</button>
+            <button onClick={next}>Next</button>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
--- src/components/layout/Sections.jsx
+++ src/components/layout/Sections.jsx
@@ -1,14 +1,6 @@
-import React, { Component } from 'react'
 import Pokemon from "../pokemon/Pokemon";
-export default class Sections extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row"><Pokemon /></div>
-            </div>
 
-        )
-    }
-}
+
+export default Pokemon;
 
 
